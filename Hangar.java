7dc6f025038f3076c983eaa6492481b23a83721e
hajarMark <66554999+hajarMark@users.noreply.github.com>
2021-04-06 17:18:49 +0200

    Update Hangar.java

--- Hangar.java
+++ Hangar.java
@@ -4,7 +4,7 @@ public class Hangar {
 	public static void main(String[] args) {
 		Car car1= new Car("Clio",25000);
 		Boat boat1=new Boat("BB",25);
-		car1.doStuff();
-		boat1.doStuff();	
+		System.out.println(car1.doStuff());
+		System.out.println(boat1.doStuff());
 	}
 }
